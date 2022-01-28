1b940e401c9d888f27a4a2f1f37adc6024df7a5d
alliesaari <alliesaari@ufl.edu>
2022-01-28 13:48:48 -0500

    Added Outlier detection/deletion to IOB

--- GetData.py
+++ GetData.py
@@ -2,12 +2,17 @@ import csv
 import time
 import datetime
 import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 file = '3_days_data_Ryan.csv'
 #file = '1_month_of_data_Ryan.csv'
 #file = '3_months_of_data_Ryan.csv'
 
-#convert date in YYYY-MM-DDTHH:MM:SS to unix timestamp in local time
+# convert date in YYYY-MM-DDTHH:MM:SS to unix timestamp in local time
+
+
 def convert_unix(s_date):
     year = int(s_date[0:4:1])
     month = int(s_date[5:7:1])
@@ -20,31 +25,53 @@ def convert_unix(s_date):
 
     return u_date
 
+
 with open(file, 'r') as data:
     csv_reader = csv.reader(data)
-    
+
     CGM_BGM = []
     IOB = []
     for line in csv_reader:
-        if len(line) > 4 :
+        if len(line) > 4:
             if line[2] == "EGV":
                 CGM_BGM.append((convert_unix(line[3]), int(line[4])))
         if len(line) > 3:
             if line[0] == "IOB":
                 convert_unix(line[2])
                 IOB.append((convert_unix(line[2]), float(line[3])))
-    
-    #lists for data points for plot
-    X=[]
-    Y=[]
+
+    # outlier detection of IOB
+
+    df = pd.DataFrame(IOB, columns=['time', 'IOB'])
+    outliers_fraction = 0.10
+    model = IsolationForest(contamination=outliers_fraction)
+    pdf = pd.DataFrame(IOB, columns=['time', 'IOB'])
+    model.fit(pdf.values)
+    pdf['anomaly2'] = pd.Series(model.predict(pdf.values))
+    # visualization
+    df['anomaly2'] = pd.Series(pdf['anomaly2'].values, index=df.index)
+    a = df.loc[df['anomaly2'] == -1]  # anomaly
+    _ = plt.figure(figsize=(18, 6))
+    _ = plt.plot(df['IOB'], color='blue', label='Normal')
+    _ = plt.plot(a['IOB'], linestyle='none', marker='X',
+                 color='red', markersize=12, label='Anomaly')
+    _ = plt.xlabel('Time')
+    _ = plt.ylabel('IOB')
+    _ = plt.title('IOB Anomalies')
+    _ = plt.legend(loc='best')
+    plt.show()
+    IOB = df.values.tolist()
+
+    # lists for data points for plot
+
+    X = []
+    Y = []
+
     for i in IOB:
-        X.append(i[0])
-        Y.append(i[1])
+        if i[2] != -1:
+            X.append(i[0])
+            Y.append(i[1])
 
     plt.scatter(X, Y, s=1)
     plt.title('IOB over time')
     plt.show()
-
-    
-
-
