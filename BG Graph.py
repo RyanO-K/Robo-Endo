1b9fc708f3aac961533868468cd03518cf385012
Jack Byrnes <jackbyrnes1998@yahoo.com>
2022-01-28 02:41:46 -0500

    BG Graph
    Signed-off-by: Jack Byrnes <jackbyrnes1998@yahoo.com>

---
+++ BG Graph.py
@@ -0,0 +1,43 @@
+import csv
+import time
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+
+file = '3_days_data_Ryan.csv'
+# file = '1_month_of_data_Ryan.csv'
+# file = '3_months_of_data_Ryan.csv'
+BG = list()
+Completion_time = list()
+
+with open(file, 'r') as data:
+    csv_reader = csv.reader(data)
+
+    for line in csv_reader:
+        if len(line) >= 41 and line[2] != "BG":
+            if line[2] == "" or line[6] == "":
+                continue
+            else:
+                BG.append(int(line[2]))
+
+                temp_str = ""
+                temp_str = temp_str + line[6][0:10] + " " + line[6][11:] + ".0"
+                date_time_obj = datetime.datetime.strptime(temp_str, '%Y-%m-%d %H:%M:%S.%f')
+                Completion_time.append(date_time_obj)
+                temp_str = ""
+
+BG = BG[1:]
+Completion_time = Completion_time[1:]
+
+plt.scatter(Completion_time, BG)
+plt.plot(Completion_time, BG)
+
+myFmt = mdates.DateFormatter('%H:%M')
+plt.gca().xaxis.set_major_formatter(myFmt)
+
+plt.title("BG level over Time in day")
+plt.xlabel("Time in day")
+plt.ylabel("Blood Glucose level (BG)")
+
+plt.show()
